da8934f48522dddf9681ac5acf9c65e9b547a0db
ErayOIsmail <147088123+ErayOIsmail@users.noreply.github.com>
2023-11-01 21:54:49 +0000

    rename of file, general though process layed out

---
+++ create_vm.py
@@ -0,0 +1,29 @@
+#import the necessary libraries required
+#figure out how to get a requirements file running idk 
+import os
+import subprocess
+#import gdown (maybe for downloading?)
+#import requests ( maybe for downaloding aswell idk)
+
+
+#make a script to find vboxmanage location
+
+#make a script to check if virtualbox is installed, if its not, prompt the user to download it.
+
+#maybe expirement with downloading files from some cloud provider? idk maybe aws
+
+#figure out how to make VMs i guess, or maybe figure out how to import them? that seems more realistic
+
+#user prompts
+#create vm function?
+#modify vm if needed
+#attach any isos maybe? for additional applications if needed ? for specialized machines maybe
+#main funciton
+def main():
+
+#have all the functions and prompts maybe in here? idk figure it out later
+
+
+#runs the script
+if __name__ == "__main__":
+  main()
